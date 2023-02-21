89e199ada651f530d832f21131d670191083c882
Gabriel Tecar <Gabriel.Tecar-3rdpty@brooks.com>
2023-02-21 12:15:42 +0200

    Added Microservices Async Communication with RabbitMQ & MassTransit

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,18 +29,29 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) //connecting Redis Docker Container with AddStackExchange
         {
+            //redis configuration
             services.AddStackExchangeRedisCache(options =>
             {
                 // CacheSettings:ConnectionString is a new configuration created in appsetings.json
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString"); //registered Redis connection, with providing the server name and port number
             });
 
+            //general configuration
             services.AddScoped<IBasketRepository, BasketRepository>();
+            services.AddAutoMapper(typeof(Startup));
 
+            //grpc configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
                 (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-
             services.AddScoped<DiscountGrpcService>();
+
+            //MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+            config.UsingRabbitMq((ctx, cfg) => {
+                cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
